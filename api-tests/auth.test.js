39c794df130910d8c8fa778a17f3627d72534ac3
Mikkel Madsen <m@madsn.net>
2017-07-26 21:04:33 +0400

    Login test

--- api-tests/auth.test.js
+++ api-tests/auth.test.js
@@ -5,3 +5,9 @@ test('Not possible to get user info when not authenticated', async () => {
   const response = await request('/user');
   expect(response).toMatchSnapshot();
 })
+
+test('Get user info when sending credentials', async () => {
+  expect.assertions(1);
+  const response = await request('/user', {auth:'user:password'});
+  expect(response).toMatchSnapshot();
+})
--- api-tests/request.js
+++ api-tests/request.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const BASE_URL = 'http://localhost:8080'
 
-export default function request(url) {
+export default function request(url, options) {
+    options = options || {};
+    options.path = BASE_URL + url;
+    options.port = 8080;
     return new Promise(resolve => {
-        http.get({path: BASE_URL + url, port: 8080}, response => {
+        http.get(options, response => {
             const { statusCode } = response;
             const contentType = response.headers['content-type'];
             let data = '';
