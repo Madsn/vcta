56a48bad34a71c04d32ba6115ec5674da74e6a0d
Mikkel Madsen <m@madsn.net>
2017-07-26 20:44:44 +0400

    Include statuscode and contenttype in jest snapshots

--- api-tests/auth.test.js
+++ api-tests/auth.test.js
@@ -1,9 +1,7 @@
 import request from './request';
 
 test('Not possible to get user info when not authenticated', async () => {
-  console.log('starting');
   expect.assertions(1);
   const response = await request('/user');
-  console.log(response);
   expect(response).toMatchSnapshot();
 })
--- api-tests/request.js
+++ api-tests/request.js
@@ -4,9 +4,11 @@ const BASE_URL = 'http://localhost:8080'
 export default function request(url) {
     return new Promise(resolve => {
         http.get({path: BASE_URL + url, port: 8080}, response => {
+            const { statusCode } = response;
+            const contentType = response.headers['content-type'];
             let data = '';
             response.on('data', _data => data += _data);
-            response.on('end', () => resolve(data));
+            response.on('end', () => resolve({data, statusCode, contentType}));
         });
     });
 }
