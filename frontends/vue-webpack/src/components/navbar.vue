a81799e3af6da58f8d68d84088c8b661c3314efe
Mikkel Madsen <m@madsn.net>
2017-07-29 11:14:38 +0400

    Replace element with bootstrap

--- frontends/vue-webpack/src/components/navbar.vue
+++ frontends/vue-webpack/src/components/navbar.vue
@@ -1,9 +1,35 @@
 <template>
-<el-menu theme="dark" :default-active="activeIndex" class="el-menu" mode="horizontal" router>
-  <el-menu-item index="/">VCTA</el-menu-item>
-  <el-menu-item index="dashboard">Dashboard</el-menu-item>
-  <el-menu-item index="scoreboard">Scoreboard</el-menu-item>
-</el-menu>
+  <b-navbar toggleable type="inverse" variant="primary">
+
+    <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+    <b-link class="navbar-brand" to="/">
+      <span>BootstrapVue</span>
+    </b-link>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-nav is-nav-bar>
+        <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+        <b-nav-item to="/scoreboard">Scoreboard</b-nav-item>
+      </b-nav>
+
+      <b-nav is-nav-bar class="ml-auto">
+
+        <b-nav-item-dropdown right>
+
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <span style="font-weight: bold;">User</span>
+          </template>
+
+          <b-dropdown-item>Profile</b-dropdown-item>
+          <b-dropdown-item>Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+      </b-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
--- frontends/vue-webpack/src/main.js
+++ frontends/vue-webpack/src/main.js
@@ -3,21 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {
-  Button,
-  Select,
-  Submenu,
-  Menu,
-  MenuItem
-} from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
-Vue.use(Button)
-Vue.use(Select)
-Vue.use(Menu)
-Vue.use(MenuItem)
-Vue.use(Submenu)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
