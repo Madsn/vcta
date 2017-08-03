c04c6eb9b7f94b844b0cfd03817b21138c1ba2e4
Mikkel Madsen <m@madsn.net>
2017-08-03 16:21:50 +0400

    Add dashboard and initial scoreboard endpoints to django

--- backends/vcta_django/vcta/settings.py
+++ backends/vcta_django/vcta/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+DEFAULT_PORT = 8080
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'channels',
     'rest_framework',
+    'drf_multiple_model',
     'corsheaders',
     'vcta_service'
 ]
--- backends/vcta_django/vcta_service/custom_views.py
+++ backends/vcta_django/vcta_service/custom_views.py
@@ -1,18 +1,26 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from drf_multiple_model.views import MultipleModelAPIView
+from .serializers import TripSerializer, UserSerializer, TeamSerializer
 
 from . import models
 
 
-class Dashboard(APIView):
+class Dashboard(MultipleModelAPIView):
 
-    def get(self, request, format=None):
-        trips = models.Trip.objects.all()
-        return Response(trips)
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        self.queryList = [
+            (models.Trip.objects.filter(user=user),TripSerializer),
+            (models.User.objects.filter(pk=user.id),UserSerializer),
+        ]
+        return super(Dashboard, self).get(request, *args, **kwargs)
 
 
-class Scoreboard(APIView):
+class Scoreboard(MultipleModelAPIView):
 
-    def get(self, request, format=None):
-        trips = models.Trip.objects.all()
-        return Response(trips)
+    def get(self, request, *args, **kwargs):
+        self.queryList = [
+            (models.Trip.objects.all(),TripSerializer),
+            (models.Team.objects.all(),TeamSerializer),
+            (models.User.objects.all(),UserSerializer),
+        ]
+        return super(Scoreboard, self).get(request, *args, **kwargs)
