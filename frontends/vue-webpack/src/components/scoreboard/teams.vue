68ff4a4f27d39ad876ac00e9156d70b962df8966
Mikkel Madsen <m@madsn.net>
2017-09-16 09:53:28 +0400

    Links to userdetails and teamdetails from scoreboard

--- frontends/vue-webpack/src/components/scoreboard/teams.vue
+++ frontends/vue-webpack/src/components/scoreboard/teams.vue
@@ -1,37 +1,41 @@
 <template>
   <table class="table table-striped">
     <thead>
-      <tr>
-        <th></th>
-        <th>Team name</th>
-        <th>Captain</th>
-        <th># of Members</th>
-        <th>Total Km</th>
-        <th>Km/members</th>
-        <th>Cycling days/members</th>
-      </tr>
+    <tr>
+      <th></th>
+      <th>Team name</th>
+      <th>Captain</th>
+      <th># of Members</th>
+      <th>Total Km</th>
+      <th>Km/members</th>
+      <th>Cycling days/members</th>
+    </tr>
     </thead>
     <tbody>
-      <tr v-for="(team, index) in teams" v-bind:key="team.id">
-        <th>{{index+1}}</th>
-        <td>{{team.name}}</td>
-        <td>{{team.captainName}}</td>
-        <td>{{team.memberCount}}</td>
-        <td>{{team.distance}}</td>
-        <td>{{team.avgDistance}}</td>
-        <td>{{team.avgDays}}</td>
-      </tr>
+    <tr v-for="(team, index) in teams" v-bind:key="team.id">
+      <th>{{index + 1}}</th>
+      <td>
+        <router-link :to="{name: 'teamdetails', params: {id: team.id}}">{{team.name}}</router-link>
+      </td>
+      <td>
+        <router-link :to="{name: 'userdetails', params: {id: team.captain}}">{{team.captainName}}</router-link>
+      </td>
+      <td>{{team.memberCount}}</td>
+      <td>{{team.distance}}</td>
+      <td>{{team.avgDistance}}</td>
+      <td>{{team.avgDays}}</td>
+    </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-export default {
-  name: 'teams',
-  props: [
-    'teams'
-  ]
-}
+  export default {
+    name: 'teams',
+    props: [
+      'teams'
+    ]
+  }
 </script>
 
 <style>
