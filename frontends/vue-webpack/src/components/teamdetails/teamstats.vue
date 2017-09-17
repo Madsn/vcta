da4071752474fa7cd3bbc0c1cedb046d97af1bd5
Mikkel Madsen <m@madsn.net>
2017-09-17 19:55:54 +0400

    Team details table

--- frontends/vue-webpack/src/components/teamdetails/teamstats.vue
+++ frontends/vue-webpack/src/components/teamdetails/teamstats.vue
@@ -1,11 +1,49 @@
 <template>
   <div>
-    <p>teamstats</p>
+    <b-card header="Team details">
+      <table class="table table-hover">
+        <thead>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Team name</td>
+          <td>{{teamInfo.name}}</td>
+        </tr>
+        <tr>
+          <td>Member count</td>
+          <td>{{teamInfo.memberCount}}</td>
+        </tr>
+        <tr>
+          <td>Captain</td>
+          <td>
+            <router-link :to="{name: 'userdetails', params: {id: teamInfo.captain}}">
+              {{teamInfo.captainName}}
+            </router-link>
+          </td>
+        </tr>
+        <tr>
+          <td>Total distance</td>
+          <td>{{teamInfo.distance}}</td>
+        </tr>
+        <tr>
+          <td>Average distance/member</td>
+          <td>{{teamInfo.avgDistance}}</td>
+        </tr>
+        <tr>
+          <td>Average days/member</td>
+          <td>{{teamInfo.avgDays}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </b-card>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'teamstats'
+    name: 'teamstats',
+    props: [
+      'teamInfo'
+    ]
   }
 </script>
